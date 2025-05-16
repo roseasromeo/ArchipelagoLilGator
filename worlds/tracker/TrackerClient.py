fa4f6780b67394a2410993cca1e50381af81cc80
Faris <faristheancient@gmail.com>
2025-05-16 18:33:18 -0500

    need to make sure item is local :pensiveclow:

--- worlds/tracker/TrackerClient.py
+++ worlds/tracker/TrackerClient.py
@@ -1127,7 +1127,7 @@ def updateTracker(ctx: TrackerGameContext) -> CurrentTrackerState:
     for item_name, item_flags, item_loc in [(item_id_to_name[item.item],item.flags,item.location) for item in ctx.tracker_items_received] + [(name,ItemClassification.progression,-1) for name in ctx.manual_items]:
         try:
             world_item = ctx.multiworld.create_item(item_name, ctx.player_id)
-            if item_loc>0:
+            if item_loc>0 and item_loc in location_id_to_name:
                 world_item.location = ctx.multiworld.get_location(location_id_to_name[item_loc],ctx.player_id)
             world_item.classification = world_item.classification | item_flags
             state.collect(world_item, True)
