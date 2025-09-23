8ab9322df07832bf08b74620ec07fab9ab075fe3
Faris <faristheancient@gmail.com>
2025-09-22 21:57:05 -0500

    Version number for 0.2.20

--- worlds/tracker/__init__.py
+++ worlds/tracker/__init__.py
@@ -20,7 +20,7 @@ def launch_client(*args):
     from .TrackerClient import launch as TCMain
     launch(TCMain, name="Universal Tracker client", args=args)
 
-UT_VERSION = "v0.2.19"
+UT_VERSION = "v0.2.20"
 
 class CurrentTrackerState(NamedTuple):
     all_items: Counter
